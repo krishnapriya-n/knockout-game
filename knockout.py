5d4adb96485bd3239eb3d5048aa037a7b4d32989
Krishna Priya Nimmagadda <101842284+krishnapriya-n@users.noreply.github.com>
2022-12-14 09:53:30 +1300

    Update knockout.py

--- knockout.py
+++ knockout.py
@@ -6,3 +6,14 @@ from time import sleep
 # INFORMATION PRINTING ##
 
 print ("KNOCKOUT GAME")
+print ("This is a dice game where each player gets to choose a 'knockout' number")
+print ("This number is either a 6, 7, 8, or 9")
+print ("More than one player can choose the same number")
+print ("Players can take turns throwing both dice, once each turn")
+print ("If the two rolled dice numbers add up to be the chosen number, the player will be eliminated")
+print ("The game will now begin")
+
+# WHILE LOOP
+
+while True:
+  
